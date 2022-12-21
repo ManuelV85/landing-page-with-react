02f0c61970a97cf6d1b944f8e99bb0e08396b51d
ManuelV85 <manuel.villater@gmail.com>
2022-12-21 03:32:28 +0000

    actualizar

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 		<footer className="py-3 mt-3 bg-dark">
 			<p className="text-center fw-bold text-muted">
-				Compyright © Your website 2021
+				Compyright © Your website 2022
 			</p>
 		</footer>
 	);
